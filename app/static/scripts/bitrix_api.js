c4a450f91979bf2812dca3fcf3bed086f1a569cd
Sleptsov Oleg <slepcov_oleg@mail.ru>
2025-11-03 23:25:37 +0700

    Added display handlers

--- app/static/scripts/bitrix_api.js
+++ app/static/scripts/bitrix_api.js
@@ -15,7 +15,7 @@ export class BitrixClient {
     }
 
     async callMethod(method, params) {
-        return await this.callMethodPromise(method, params).json();
+        return await this.callMethodPromise(method, params);
     }
 
     async runBP(templateId, documentId, params = {}) {
