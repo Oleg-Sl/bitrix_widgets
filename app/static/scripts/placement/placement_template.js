ee4373b842639419ca46206bc2d62e75e00466ea
Sleptsov Oleg <slepcov_oleg@mail.ru>
2025-11-04 16:18:10 +0700

    Added display handlers

--- app/static/scripts/placement/placement_template.js
+++ app/static/scripts/placement/placement_template.js
@@ -1,7 +1,51 @@
 
 
 export class PlacementTemplate {
-    static createTable(eventsData) {
+    static createPlacementsHTML(availableEvents, registeredEvents) {
+        return `
+            ${this.createStyles()}
+            <div class="bx24_events__table_events_container">
+                ${this.createRegisteredEventsTable(registeredEvents)}
+            </div>
+            <div class="bx24_events__registry_event_container">
+                ${this.createAvailableEventsTable(availableEvents)}
+            </div>
+        `;
+    }
+
+    static createAvailableEventsTable(availableEvents) {
+        return `
+            <h4>Регистрация событий</h4>
+            <div class="border border-1 rounded p-3">
+                <div class="bx24_events__type_event">
+                    <label for="registry_event__selectTypeEvent" class="form-label">Тип события</label>
+                    <select class="form-select bx24_events__registry_event_select" aria-label="Default select example" id="registry_event__selectTypeEvent">
+                        <option value="online" selected>Онлайн</option>
+                        <option value="offline">Оффлайн</option>
+                    </select>    
+                </div>
+                <div class="bx24_events__name_event">
+                    <label for="name_event__selectTypeEvent" class="form-label">Название события</label>
+                    <select class="form-select" aria-label="Default select example" id="name_event__selectTypeEvent">
+                        ${this.getOptionsHTML(availableEvents)}
+                    </select>    
+                </div>
+                <div class="bx24_events__handler_event">
+                    <label for="name_event__handlerEvent" class="form-label">URL обработчика</label>
+                    <input class="form-control" type="text" placeholder="..." aria-label="input example" id="name_event__handlerEvent">
+                </div>
+                <div class="bx24_events__source_event">
+                    <label for="name_event__sourceKey" class="form-label">Ключ источника</label>
+                    <input class="form-control" type="text" placeholder="..." aria-label="input example" id="name_event__sourceKey">
+                </div>
+                <div class="d-grid gap-2 d-md-flex justify-content-md-end mt-2 bx24_events__add_event">
+                    <button class="btn btn-primary me-md-2" type="button">Добавить</button>
+                </div>
+            </div>
+        `;
+    }
+
+    static createRegisteredEventsTable(events) {
         let content = `
             <table class="table table-hover table-bordered caption-top bx24_events__table_events">
                 <caption>Список установленных обработчиков событий</caption>
@@ -15,22 +59,22 @@ export class PlacementTemplate {
                     </tr>
                 </thead>
                 <tbody>
-                    ${this.generateTbodyHTML(eventsData)}
+                    ${this.generateTbodyHTML(events)}
                 </tbody>
             </table>
         `;
         return content;
     }
 
-    static generateTbodyHTML(eventsData) {
+    static generateRegisteredEventsTbodyHTML(eventsData) {
         let content = '';
         for (let eventData of eventsData) {
-            content += this.getRowEventHTML(eventData.event, eventData.offline, eventData.handler, eventData.connector_id);
+            content += this.getRowRegisteredEventsTbodyHTML(eventData.event, eventData.offline, eventData.handler, eventData.connector_id);
         }
         return content
     }
     
-    static getRowEventHTML(name, isOffline, handler, connectorId) {
+    static getRowRegisteredEventsTbodyHTML(name, isOffline, handler, connectorId) {
         let typeEventRus = isOffline ? "оффлайн" : "онлайн";
         let typeEventEng = isOffline ? "offline" : "online";
         return `
@@ -48,9 +92,8 @@ export class PlacementTemplate {
         `;
     }
 
-    static createPlacementsHTML(availableEvents, eventsList) {
-        const contentRegistredTableHTML = this.createTable(availableEvents);
-        let content = `
+    static createStyles() {
+        return `
             <style type="text/css">
                 .bx24_events__table_events_remove_row {
                     width: 100%;
@@ -73,41 +116,7 @@ export class PlacementTemplate {
                     color: #000000;
                 }
             </style>
-            <div class="bx24_events__table_events_container">
-                ${contentRegistredTableHTML}
-            </div>
-    
-            <div class="bx24_events__registry_event_container">
-                <h4>Регистрация событий</h4>
-                <div class="border border-1 rounded p-3">
-                    <div class="bx24_events__type_event">
-                        <label for="registry_event__selectTypeEvent" class="form-label">Тип события</label>
-                        <select class="form-select bx24_events__registry_event_select" aria-label="Default select example" id="registry_event__selectTypeEvent">
-                            <option value="online" selected>Онлайн</option>
-                            <option value="offline">Оффлайн</option>
-                        </select>    
-                    </div>
-                    <div class="bx24_events__name_event">
-                        <label for="name_event__selectTypeEvent" class="form-label">Название события</label>
-                        <select class="form-select" aria-label="Default select example" id="name_event__selectTypeEvent">
-                            ${this.getOptionsHTML(eventsList)}
-                        </select>    
-                    </div>
-                    <div class="bx24_events__handler_event">
-                        <label for="name_event__handlerEvent" class="form-label">URL обработчика</label>
-                        <input class="form-control" type="text" placeholder="..." aria-label="input example" id="name_event__handlerEvent">
-                    </div>
-                    <div class="bx24_events__source_event">
-                        <label for="name_event__sourceKey" class="form-label">Ключ источника</label>
-                        <input class="form-control" type="text" placeholder="..." aria-label="input example" id="name_event__sourceKey">
-                    </div>
-                    <div class="d-grid gap-2 d-md-flex justify-content-md-end mt-2 bx24_events__add_event">
-                        <button class="btn btn-primary me-md-2" type="button">Добавить</button>
-                    </div>
-                </div>
-            </div>
         `;
-        return content;
     }
 
     static getOptionsHTML(eventsList) {
